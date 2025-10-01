0e0966ec07bc831ed0f0414b02de43aed74b9a84
Kockstik <kostyadi04@mail.ru>
2025-10-01 15:18:39 +0700

    Create safari widget

---
+++ Main/Views/Elements/SafariView.swift
@@ -0,0 +1,19 @@
+//
+//  SafariView.swift
+//  Outline
+//
+//  Created by Diesperov Konstantin on 28.09.2025.
+//
+
+import SwiftUI
+import SafariServices
+
+struct SafariView: UIViewControllerRepresentable {
+    let url: URL
+
+    func makeUIViewController(context: Context) -> SFSafariViewController {
+        return SFSafariViewController(url: url)
+    }
+
+    func updateUIViewController(_ uiViewController: SFSafariViewController, context: Context) {}
+}
