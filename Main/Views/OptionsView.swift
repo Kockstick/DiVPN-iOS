b688bae6936df291fa1978dd8828fc35271618b4
Kockstik <kostyadi04@mail.ru>
2026-01-19 12:32:44 +0700

    Move information to OptionsInfo

--- Main/Views/OptionsView.swift
+++ Main/Views/OptionsView.swift
@@ -31,28 +31,7 @@ struct OptionsView: View {
                 VStack{
                     Spacer()
                     
-                    HStack(spacing: 15){
-                        Image("VerticalDivider")
-                            .resizable()
-                            .frame(width: 6, height: 70)
-                            .foregroundColor(Color("TextSecondary"))
-                            
-                        VStack{
-                            Text(user?.email ?? "user")
-                                .foregroundColor(Color("TextSecondary"))
-                                .font(.body).bold()
-                                .frame(maxWidth: .infinity, alignment: .leading)
-                                .multilineTextAlignment(.leading)
-                            
-                            Text(ssManager.serverLocation ?? "• • • • • •")
-                                .font(.largeTitle).bold()
-                                .frame(maxWidth: .infinity, alignment: .leading)
-                                .multilineTextAlignment(.leading)
-                                .shimmer(ssManager.serverLocation == nil, color: Color("TextSecondary"))
-                        }
-                        .padding(.vertical, 5)
-                    }
-                    .fixedSize(horizontal: false, vertical: true)
+                    OptionsInfo(ssManager: ssManager, user: $user)
                     
                     Spacer()
                         .frame(maxHeight: 40)
